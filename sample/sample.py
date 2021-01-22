2a93ad786d115b58d9997fc170024911ad9a4059
acpguedes <guedes.aureliano@gmail.com>
2021-01-22 16:19:03 -0300

    app finalized

--- sample/sample.py
+++ sample/sample.py
@@ -11,8 +11,8 @@ less_twenty_days = timedelta(20)
 ten_days_before = today - less_ten_days
 twenty_days_before = today - less_twenty_days
 
-ten_days_before = ten_days_before.isocalendar()
-twenty_days_before = twenty_days_before.isocalendar()
+ten_days_before = ten_days_before.date().isoformat()
+twenty_days_before = twenty_days_before.date().isoformat()
 
 cambio = BCCambio()
 cambio1 = cambio.get_currency(ten_days_before)
@@ -26,8 +26,10 @@ print(cambio2.status_code)
 print(cambio2.url)
 print(cambio2.text)
 
-report = BCReport()
-report1 = report(ten_days_before)
-report2 = report(twenty_days_before, ten_days_before)
+report = BCReport(ten_days_before)
+report1 = report.report
 print(report1)
+
+report = BCReport(twenty_days_before, ten_days_before)
+report2 = report.report
 print(report2)
---
+++ script/get_currency.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from BCScrapper.bcscrapper import BCReport
+
+first_day = '10-10-2020'
+last_day = '10-01-2021'
+output = 'example.csv'
+
+report = BCReport(first_day, last_day)
+
+report.report.to_csv(output)
